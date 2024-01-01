2b1d024a4f6fce42a2c8230da5fdac2b8f265410
Anand kushwaha <anandkushwaha@Anands-Mac-mini.local>
2024-01-01 23:39:57 +0530

    added roles and to User

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ const { PORT } = require("./config/serverConfig");
 const app = express();
 const apiRoutes = require("./routes/index.js");
 const bodyParser = require("body-parser");
-
+const db = require("./models/index.js");
 const prepareAndStartServer = () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use("/api", apiRoutes);
   app.listen(3001, () => {
     console.log("Server started at", PORT);
+    // if (process.env.DB_SYNC) {
+    //   db.sequelize.sync({ alter: true });
+    // }
   });
 };
 
